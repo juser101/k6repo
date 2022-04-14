f2bc9be4c6fc4ea04fba9f8aa8353da5139882e0
EC2 Default User <ec2-user@ip-10-0-3-1.us-west-2.compute.internal>
2022-04-14 13:47:04 +0000

    added epel in setup

--- setup_k6.sh
+++ setup_k6.sh
@@ -3,9 +3,5 @@ set -ex
 
 cat setup_k6.sh
 sudo yum update -y
-
-sudo amazon-linux-extras install epel
-sudo yum install snapd -y
-sudo systemctl enable --now snapd.socket
-sudo ln -s /var/lib/snapd/snap /snap
-sudo snap install k6
+sudo yum install https://dl.k6.io/rpm/repo.rpm
+sudo yum install k6  --nogpgcheck 
