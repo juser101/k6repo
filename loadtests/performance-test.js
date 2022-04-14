156f74f9735e3b56ecaca6f2eb3cd78c0a48bb0a
EC2 Default User <ec2-user@ip-10-0-3-1.us-west-2.compute.internal>
2022-04-14 14:47:53 +0000

    added epel in setup

--- loadtests/performance-test.js
+++ loadtests/performance-test.js
@@ -6,10 +6,11 @@ export let options = {
   vus: 50,
   thresholds: {
     http_req_duration: ["p(95)<500"] // 95 percent of response times must be below 500ms
+    http_req_failed: ['rate<0.01'] // During the whole test execution, the error rate must be lower than 1%.
   }
 };
 
 export default function() {
-  http.get("http://test.k6.io/contacts.php");
+  http.get("http://test.k6.io/contactss.php");
   sleep(3);
 };
