1af971c26ddafb9fcb23617c949d3b4da57e293d
EC2 Default User <ec2-user@ip-10-0-3-1.us-west-2.compute.internal>
2022-04-14 13:50:49 +0000

    added epel in setup

--- setup_k6.sh
+++ setup_k6.sh
@@ -3,5 +3,5 @@ set -ex
 
 cat setup_k6.sh
 sudo yum update -y
-sudo yum install https://dl.k6.io/rpm/repo.rpm
-sudo yum install k6  --nogpgcheck 
+sudo yum install https://dl.k6.io/rpm/repo.rpm -y
+sudo yum install k6  --nogpgcheck  -y
