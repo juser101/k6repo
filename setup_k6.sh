12a49694aa36568de6c0c6ad6894d8481075a9b4
EC2 Default User <ec2-user@ip-10-0-3-1.us-west-2.compute.internal>
2022-04-14 13:37:20 +0000

    added yum install

--- setup_k6.sh
+++ setup_k6.sh
@@ -4,7 +4,8 @@ set -ex
 cat setup_k6.sh
 sudo yum update -y
 
-sudo yum  install https://dl.k6.io/rpm/repo.rpm -y
-sudo yum install k6 -y 
-
- 
+sudo yum install epel-release -y
+sudo yum install snapd -y
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install k6
