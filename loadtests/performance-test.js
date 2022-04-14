45753e9ff7ebcad72192b1ea246e2060e3cea5e0
EC2 Default User <ec2-user@ip-10-0-3-1.us-west-2.compute.internal>
2022-04-14 14:52:10 +0000

    added epel in setup

--- loadtests/performance-test.js
+++ loadtests/performance-test.js
@@ -5,7 +5,7 @@ export let options = {
   duration: "1m",
   vus: 50,
   thresholds: {
-    http_req_duration: ["p(95)<500"] // 95 percent of response times must be below 500ms
+    http_req_duration: ["p(95)<500"], // 95 percent of response times must be below 500ms
     http_req_failed: ['rate<0.01'] // During the whole test execution, the error rate must be lower than 1%.
   }
 };
